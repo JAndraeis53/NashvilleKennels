274006b7ada01cb6934f7b514b3307e8e473b38d
Jalon Duncan <duncanjalon@gmail.com>
2021-07-09 15:15:35 -0500

    EOD bug hunt

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -25,4 +25,4 @@ export const AnimalList = () => {
         }
         </div>
     )
-    }
+}
--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Location.css"
 
-export const LocationCard = () => (
+export const LocationCard = ({ location }) => (
     <section className="location">
-        <h3 className="location__name">Home Base</h3>
-        <div className="Location_type">Type: Kennel</div>
+        <h3 className="location__name">{location.name}</h3>
+        <div className="Location_type">{location.address}</div>
     </section>
 )
